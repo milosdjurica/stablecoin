45975f623176ffcc8a5378a089804c24d71584a8
milosdjurica <milosdjurica13@gmail.com>
2023-12-25 15:33:45 +0100

    tokenAddresses and priceFeedAddresses arrays added

--- contracts/CSCEngine.sol
+++ contracts/CSCEngine.sol
@@ -14,6 +14,7 @@ contract CSCEngine {
     ////////////////////
     // * Errors 	  //
     ////////////////////
+    error CSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
 
     ////////////////////
     // * Types 		  //
@@ -23,6 +24,10 @@ contract CSCEngine {
     // * Variables	  //
     ////////////////////
     CharityStableCoin private immutable i_csc;
+
+    mapping(address token => address priceFeed) private s_priceFeeds;
+
+    address[] private s_collateralDeposited;
 
     ////////////////////
     // * Events 	  //
@@ -39,7 +44,15 @@ contract CSCEngine {
     ////////////////////
     // * Constructor  //
     ////////////////////
-    constructor(address cscAddress) {
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address cscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert CSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralDeposited.push(tokenAddresses[i]);
+        }
         i_csc = CharityStableCoin(cscAddress);
     }
 
