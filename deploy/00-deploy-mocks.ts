c0cf2ae65d4e968f2b1661b401737b1e021d9b58
milosdjurica <milosdjurica13@gmail.com>
2023-12-25 16:12:33 +0100

    deploy mocks script

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,38 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { developmentChains } from "../utils/helper.config";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	const { getNamedAccounts, deployments, network } = hre;
+	const { deployer } = await getNamedAccounts();
+	const { deploy, log } = deployments;
+
+	const DECIMALS = 8;
+	const ETH_USD_PRICE = 2000e8;
+	const BTC_USD_PRICE = 20000e8;
+
+	if (developmentChains.includes(network.name)) {
+		console.log("Local network detected! Deploying mocks...");
+
+		const ethMock = await deploy("MockV3Aggregator", {
+			from: deployer,
+			args: [DECIMALS, ETH_USD_PRICE],
+			log: true,
+		});
+		log(`EthMock contract: `, ethMock.address);
+
+		const btcMock = await deploy("MockV3Aggregator", {
+			from: deployer,
+			args: [DECIMALS, BTC_USD_PRICE],
+			log: true,
+		});
+		log(`BtcMock contract: `, btcMock.address);
+
+		log("Mocks deployed!!!");
+		log("===============================================================");
+	}
+};
+
+export default func;
+func.id = "deploy_mockV3Aggregator"; // id required to prevent re-execution
+func.tags = ["mockV3Aggregator", "all"];
