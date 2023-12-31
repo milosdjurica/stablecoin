efe30ad3eb612c3e2d82f00c86616dd6047e33e8
milosdjurica <milosdjurica13@gmail.com>
2023-12-31 20:23:19 +0100

    deposit collateral func

--- contracts/CSCEngine.sol
+++ contracts/CSCEngine.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.20;
 ////////////////////
 
 import {CharityStableCoin} from "./CharityStableCoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
@@ -15,6 +16,7 @@ contract CSCEngine {
     // * Errors 	  //
     ////////////////////
     error CSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error CSCEngine__TransactionFailed();
 
     ////////////////////
     // * Types 		  //
@@ -23,9 +25,12 @@ contract CSCEngine {
     ////////////////////
     // * Variables	  //
     ////////////////////
+
     CharityStableCoin private immutable i_csc;
-    address private immutable i_collateralAddress;
+    address private immutable i_tokenCollateralAddress;
     address private immutable i_priceFeedAddress;
+
+    mapping(address => uint256) s_collateralDeposited;
 
     ////////////////////
     // * Events 	  //
@@ -43,7 +48,7 @@ contract CSCEngine {
     // * Constructor  //
     ////////////////////
     constructor(address _tokenAddress, address _priceFeedAddress, address _cscAddress) {
-        i_collateralAddress = _tokenAddress;
+        i_tokenCollateralAddress = _tokenAddress;
         i_priceFeedAddress = _priceFeedAddress;
         i_csc = CharityStableCoin(_cscAddress);
     }
@@ -59,6 +64,11 @@ contract CSCEngine {
     ////////////////////
     // * Public 	  //
     ////////////////////
+    function depositCollateral(uint256 _amount) public {
+        s_collateralDeposited[msg.sender] += _amount;
+        bool success = IERC20(i_tokenCollateralAddress).transferFrom(msg.sender, address(this), _amount);
+        if (!success) revert CSCEngine__TransactionFailed();
+    }
 
     ////////////////////
     // * Internal 	  //
@@ -73,7 +83,7 @@ contract CSCEngine {
     ////////////////////
 
     function getCollateralTokenAddress() external view returns (address) {
-        return i_collateralAddress;
+        return i_tokenCollateralAddress;
     }
 
     function getPriceFeedAddress() external view returns (address) {
